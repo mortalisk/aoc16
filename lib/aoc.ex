702cce7a394aeca0d8a328eb0a9bb0d8a2f18dbb
Morten Bendiksen <morten.bendiksen@gmail.com>
2016-12-03 23:28:54 +0100

    remove debugging

--- lib/aoc.ex
+++ lib/aoc.ex
@@ -5,14 +5,14 @@ defmodule Aoc do
     |> String.split(", ")
     |> Enum.reduce({:n, 0},
         fn
-          ("L" <> steps, {:n, total}) -> {:w, total - String.to_integer(IO.inspect(steps))}
-          ("L" <> steps, {:w, total}) -> {:s, total - String.to_integer(IO.inspect(steps))}
-          ("L" <> steps, {:s, total}) -> {:e, total + String.to_integer(IO.inspect(steps))}
-          ("L" <> steps, {:e, total}) -> {:n, total + String.to_integer(IO.inspect(steps))}
-          ("R" <> steps, {:n, total}) -> {:e, total + String.to_integer(IO.inspect(steps))}
-          ("R" <> steps, {:w, total}) -> {:n, total + String.to_integer(IO.inspect(steps))}
-          ("R" <> steps, {:s, total}) -> {:w, total - String.to_integer(IO.inspect(steps))}
-          ("R" <> steps, {:e, total}) -> {:s, total - String.to_integer(IO.inspect(steps))}
+          ("L" <> steps, {:n, total}) -> {:w, total - String.to_integer(steps)}
+          ("L" <> steps, {:w, total}) -> {:s, total - String.to_integer(steps)}
+          ("L" <> steps, {:s, total}) -> {:e, total + String.to_integer(steps)}
+          ("L" <> steps, {:e, total}) -> {:n, total + String.to_integer(steps)}
+          ("R" <> steps, {:n, total}) -> {:e, total + String.to_integer(steps)}
+          ("R" <> steps, {:w, total}) -> {:n, total + String.to_integer(steps)}
+          ("R" <> steps, {:s, total}) -> {:w, total - String.to_integer(steps)}
+          ("R" <> steps, {:e, total}) -> {:s, total - String.to_integer(steps)}
         end)
   end
 end
