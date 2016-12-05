b76f85360f0898561f5dbb485b756f885e22108c
Morten Bendiksen <morten.bendiksen@gmail.com>
2016-12-05 23:07:38 +0100

    day 3 part 2

--- lib/n3.ex
+++ lib/n3.ex
@@ -11,4 +11,16 @@ defmodule N3 do
     |> Enum.count
     |> IO.puts
   end
+
+  def solve2 do
+    "input3"
+    |> File.stream!()
+    |> Stream.flat_map(&String.split/1)
+    |> Stream.map(&String.to_integer/1)
+    |> Stream.chunk(9)
+    |> Stream.flat_map(fn [a1, b1, c1, a2, b2, c2, a3, b3, c3] -> [[a1, a2, a3], [b1, b2, b3], [c1, c2, c3]] end)
+    |> Stream.filter(&is_triangle/1)
+    |> Enum.count
+    |> IO.puts
+  end
 end
