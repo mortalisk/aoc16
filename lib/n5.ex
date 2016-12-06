6dcfafeb5f733b9b6829d519c6c9a0d2a9404339
Morten Bendiksen <morten.bendiksen@vimond.com>
2016-12-06 19:26:05 +0100

    day 5 part 2 (incomplete)

--- lib/n5.ex
+++ lib/n5.ex
@@ -17,4 +17,29 @@ defmodule N5 do
     |> IO.inspect()
   end
 
+  def solve2 do
+    Stream.iterate(0, &(&1+1))
+    |> Stream.map(&md5/1)
+    |> Stream.filter(&(String.starts_with?(&1, "00000")))
+    |> Stream.map(fn(hash) ->
+            i = hash
+                |> String.at(5)
+                |> Integer.parse()
+             case i do
+               :error -> :error
+               {i, _} -> {i, String.at(hash, 6)}
+             end
+          end)
+    |> Stream.filter(fn
+            (:error) -> false
+            (_) -> true
+          end)
+    |> Enum.take(8)
+    |> Enum.sort_by(&(elem(&1, 0)))
+    |> Enum.map(&(elem(&1, 1)))
+    |> Enum.to_list()
+    |> to_string()
+    |> IO.inspect()
+  end
+
 end
