e342ee8feb69f8078ecd9a227023ac3cb3b81bd1
Morten Bendiksen <morten.bendiksen@gmail.com>
2016-12-06 12:34:38 +0100

    --amend

--- lib/n6.ex
+++ lib/n6.ex
@@ -2,25 +2,26 @@ defmodule N6 do
 
   def solve do
     freq=
-      "input6"
+      "testin"
       |> File.stream!()
       |> Stream.map(&String.graphemes/1)
       |> Stream.map(&Enum.with_index/1)
       |> Enum.reduce(%{}, fn(str, map) ->
            Enum.reduce(str, map, fn({char, i}, map) ->
-             Map.update(map, i, %{}, fn(charmap) -> Map.update(charmap, char, 1, &(&1 + 1)) end)
+             Map.update(map, i, %{char => 1}, fn(charmap) -> Map.update(charmap, char, 1, &(&1 + 1)) end)
            end)
          end)
     n = Enum.count(freq) - 1
+    IO.inspect(freq)
 
     0..n
     |> Enum.reduce([], fn(i, acc) ->
           char =
-            Enum.reduce(freq[i], {?a, 0}, fn({c, n}, {bigc, bign}) ->
-              if n > bign do
+            Enum.reduce(freq[i], {?z, 0}, fn({c, n}, {smallc, smalln}) ->
+              if n > smalln do
                 {c, n}
               else
-                {bigc, bign}
+                {smallc, smalln}
               end
             end)
             |> elem(0)
